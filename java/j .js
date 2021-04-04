15561254a608c19454b2209f1487888230be71eb
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-04-05 01:12:03 +0300

    lab

---
+++ java/j .js
@@ -0,0 +1,20 @@
+alert("email is (mohammad)");
+
+var email = prompt('enter your email');
+alert("age is (25)");
+var age = prompt('enter your age');
+alert ("mohammad is your name");
+var nam = prompt("enter your name");
+alert("what is date ");
+var date = prompt("enter your date");
+
+
+document.write('<h3>'+email+'</h3>');
+document.write('<h3>'+age+'</h3>');
+document.write('<h3>'+nam+'</h3>');
+document.write('<h3>'+date+'</h3>');
+
+
+function hallo(num)
+{return"hello"+num}
+hallo("mohammad");
