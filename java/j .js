a5a5efe9c212698774f8f0b1a5553e4b834cf832
mohammad ahmad theeb hayajneh <mohammadhayagneh96@gmail.com>
2021-04-05 11:55:48 +0300

    pro

--- java/j .js
+++ java/j .js
@@ -1,5 +1,4 @@
 alert("email is (mohammad)");
-
 var email = prompt('enter your email');
 alert("age is (25)");
 var age = prompt('enter your age');
@@ -8,13 +7,7 @@ var nam = prompt("enter your name");
 alert("what is date ");
 var date = prompt("enter your date");
 
-
-document.write('<h3>'+email+'</h3>');
-document.write('<h3>'+age+'</h3>');
-document.write('<h3>'+nam+'</h3>');
-document.write('<h3>'+date+'</h3>');
+document.write('<h3>'+email+'</h3>');  document.write('<h3>'+age+'</h3>');
+document.write('<h3>'+nam+'</h3>');   document.write('<h3>'+date+'</h3>');
 
 
-function hallo(num)
-{return"hello"+num}
-hallo("mohammad");
